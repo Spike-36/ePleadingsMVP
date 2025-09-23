db683be8bad321ce6f06d7bb1df767f521ce2472
Spike-36 <peter.milligan@mac.com>
2025-09-23 22:50:46 +0900

    Initial Commit

---
+++ ePleadingsMVP/ePleadingsMVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ePleadingsMVPApp.swift
+//  ePleadingsMVP
+//
+//  Created by Peter Milligan on 23/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ePleadingsMVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
